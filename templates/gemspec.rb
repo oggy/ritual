53b5e2263376d5e6dc0368f22cf78279f37ad923
George Ogata <george.ogata@gmail.com>
2015-02-02 03:26:55 -0500

    Move ritual dependency to Gemfile.

--- templates/gemspec.rb
+++ templates/gemspec.rb
@@ -1,4 +1,3 @@
-# -*- encoding: utf-8 -*-
 $:.unshift File.expand_path('lib', File.dirname(__FILE__))
 require '<%= gem_name %>/version'
 
@@ -15,6 +14,4 @@ Gem::Specification.new do |gem|
   gem.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   gem.files         = `git ls-files`.split("\n")
   gem.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
-
-  gem.add_development_dependency 'ritual', '~> <%= ritual_version %>'
 end
