23449c93799500483f1ad63891755676ab88b4d9
Billy.Zheng <vil963@gmail.com>
2022-04-23 01:12:18 +0800

    Support whitespace before LATEST title

--- lib/ritual/changelog.rb
+++ lib/ritual/changelog.rb
@@ -13,7 +13,7 @@ module Ritual
         raise Error, "Don't release without a CHANGELOG!"
       text = File.read(path)
       heading = "#{version} #{Date.today.strftime('%Y-%m-%d')}"
-      text.sub!(/^(== )LATEST$/i, "\\1#{heading}") or
+      text.sub!(/^(\s*== )LATEST$/i, "\\1#{heading}") or
         raise Error, "No LATEST entry in CHANGELOG - did you forget to update it?"
       open(path, 'w'){|f| f.print text}
     end
